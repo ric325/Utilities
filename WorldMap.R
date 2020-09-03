68fbdd8d2789178e2dbc3e58e1738cb3acb633b2
Anthony Richardson <anthony.richardson@csiro.au>
2020-09-03 11:59:05 +1000

    rnaturalearthdata and rgeos packages needed
    I found that these 2 packages needed to be installed to run it

--- WorldMap.R
+++ WorldMap.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(sf)
 library(rnaturalearth)
+library(rnaturalearthdata)
+library(rgeos)
 
 robCRS <- "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 lonlatCRS <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
